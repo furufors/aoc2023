08588d2796950da187a9ca5501c5235229cf93fc
Fredrik Furufors <furufors@gmail.com>
2023-12-02 06:41:45 +0100

    Added day 2

--- day02/day2p2.hs
+++ day02/day2p2.hs
@@ -1,4 +1,44 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
+import Text.Parsec
+import Text.Parsec.Char
+
 main :: IO ()
-main = undefined
+main = interact $ show . sum . map (toPower (0,0,0) . snd . parseline) . lines
+
+toPower :: (Int,Int,Int) -> [(Int,Int,Int)] -> Int
+toPower (r,g,b) [] = r*g*b
+toPower (r,g,b) ((r1,g1,b1):rs) = toPower (max r r1, max g g1, max b b1) rs
+
+constraint :: (Int,Int,Int) -> (Int, [(Int,Int,Int)]) -> Bool
+constraint _ (i,[]) = True
+constraint (r,g,b) (i,((r1,g1,b1):rs)) = and [r1 <= r, g1 <= g, b1 <= b, constraint (r,g,b) (i,rs)]
+
+parseline :: String -> (Int, [(Int,Int,Int)])
+parseline s = let (i, ps) = fromRight (error "parse fail") . parse numbers "" $ s
+    in (i, map combine ps)
+    where
+        numbers = do
+            _ <- string "Game "
+            i <- read <$> many1 digit
+            _ <- string ": "
+            counts <- (colour `sepBy` string ", ") `sepBy` (string "; ")
+            return (i, counts)
+        colour = try red <|> try green <|> try blue
+        red = do
+            i <- read <$> many1 digit
+            _ <- string " red"
+            return (i, 0, 0)
+        green = do
+            i <- read <$> many1 digit
+            _ <- string " green"
+            return (0, i, 0)
+        blue = do
+            i <- read <$> many1 digit
+            _ <- string " blue"
+            return (0, 0, i)
+
+combine :: [(Int,Int,Int)] -> (Int,Int,Int)
+combine [] = (0,0,0)
+combine ((r,g,b):rs) = let (r1, g1, b1) = combine rs
+                       in (r+r1, g+g1, b+b1)

2021eab40ffc3135e62770218e8f489efb5bb02b
Fredrik Furufors <furufors@gmail.com>
2023-12-03 07:34:06 +0100

    Clean up day 3

--- day03/day3p1.hs
+++ day03/day3p1.hs
@@ -5,21 +5,20 @@ main :: IO ()
 main = interact $ show . sum . map fst . filter symbolAdjacent . toAdjacents . lines
 
 toAdjacents :: [[Char]] -> [(Int, [Char])]
-toAdjacents css =
-    let height = length css
-        width = length (head css)
-    in toA width height 0 0 []
+toAdjacents css = toA 0 0 []
     where
-        toA :: Int -> Int -> Int -> Int -> [(Int, [Char])] -> [(Int, [Char])]
-        toA w h x y acc | x + 1 >= w && y + 1 >= h = acc
-        toA w h x y acc | x + 1 >= w = toA w h 0 (y + 1) acc
-        toA w h x y acc = if css!!y!!x `elem` ['0'..'9']
-                          then let str = drop x (css!!y)
-                                   i = takeWhile (`elem` ['0'..'9']) str
-                                   l = length i
-                                   ns = [ css!!y!!x | y <- [y-1, y, y+1], x <-[(x-1)..(x+l)], x >= 0, x < w, y >= 0, y < h]
-                               in toA w h (x+l) y ((read i, ns):acc)
-                          else toA w h (x+1) y acc
+        h = length css
+        w = length (head css)
+        toA :: Int -> Int -> [(Int, [Char])] -> [(Int, [Char])]
+        toA x y acc | x + 1 >= w && y + 1 >= h = acc
+        toA x y acc | x + 1 >= w = toA 0 (y + 1) acc
+        toA x y acc = if css!!y!!x `elem` ['0'..'9']
+                      then let str = drop x (css!!y)
+                               i = takeWhile (`elem` ['0'..'9']) str
+                               l = length i
+                               ns = [ css!!y!!x | y <- [y-1, y, y+1], x <-[(x-1)..(x+l)], x >= 0, x < w, y >= 0, y < h]
+                           in toA (x+l) y ((read i, ns):acc)
+                      else toA (x+1) y acc
 
 symbolAdjacent :: (Int, [Char]) -> Bool
 symbolAdjacent (i, cs) = let remaining = length $ filter (\c -> not $ c `elem` ('.':['0'..'9'])) cs
--- day03/day3p2.hs
+++ day03/day3p2.hs
@@ -9,20 +9,19 @@ sumGears [] = 0
 sumGears ((i,ps):rs) = sum [i * x | star <- ps, (x, os) <- rs, ostar <- os, star == ostar] + sumGears rs
 
 toAdjacents :: [[Char]] -> [(Int, [(Int,Int)])]
-toAdjacents css =
-    let height = length css
-        width = length (head css)
-    in toA width height 0 0 []
+toAdjacents css = toA 0 0 []
     where
-        toA :: Int -> Int -> Int -> Int -> [(Int, [(Int,Int)])] -> [(Int, [(Int,Int)])]
-        toA w h x y acc | x + 1 >= w && y + 1 >= h = acc
-        toA w h x y acc | x + 1 >= w = toA w h 0 (y + 1) acc
-        toA w h x y acc = if css!!y!!x `elem` ['0'..'9']
-                          then let str = drop x (css!!y)
-                                   i = takeWhile (`elem` ['0'..'9']) str
-                                   l = length i
-                                   ns = [ (x,y) | y <- [y-1, y, y+1], x <-[(x-1)..(x+l)], x >= 0, x < w, y >= 0, y < h, css!!y!!x == '*']
-                                   acc' =  if length ns > 0 then ((read i, ns):acc) else acc
-                               in toA w h (x+l) y acc'
-                          else toA w h (x+1) y acc
+        h = length css
+        w = length (head css)
+        toA :: Int -> Int -> [(Int, [(Int,Int)])] -> [(Int, [(Int,Int)])]
+        toA x y acc | x + 1 >= w && y + 1 >= h = acc
+        toA x y acc | x + 1 >= w = toA 0 (y + 1) acc
+        toA x y acc = if css!!y!!x `elem` ['0'..'9']
+                      then let str = drop x (css!!y)
+                               i = takeWhile (`elem` ['0'..'9']) str
+                               l = length i
+                               ns = [ (x,y) | y <- [y-1, y, y+1], x <-[(x-1)..(x+l)], x >= 0, x < w, y >= 0, y < h, css!!y!!x == '*']
+                               acc' =  if length ns > 0 then ((read i, ns):acc) else acc
+                           in toA (x+l) y acc'
+                      else toA (x+1) y acc
 

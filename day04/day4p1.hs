5ffe3f024a6b3d0252a44bdb347fa7ff3dd72e6b
Fredrik Furufors <furufors@gmail.com>
2023-12-04 20:48:11 +0100

    Clean up day 4

--- day04/day4p1.hs
+++ day04/day4p1.hs
@@ -1,6 +1,5 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
-import Debug.Trace
 import Data.Either
 import Text.Parsec
 import Text.Parsec.Char
@@ -18,27 +17,14 @@ parseline :: String -> (Int, [Int], [Int])
 parseline = fromRight (error "parse fail") . parse round ""
     where
         round = do
-            _ <- string "Card "
-            _ <- spaces
+            string "Card" >> spaces
             i <- read <$> many1 digit
-            _ <- string ": "
+            string ": "
             hand <- many1 (noneOf "|") `sepBy`string "|"
-            --_ <- string " | "
-            --winners <- (read <$> (spaces >> many1 digit)) `sepBy`string " "
-            pure (i, trace (show hand) $ parseNs . trim $ hand!!0, parseNs . trim $ hand!!1)
+            pure (i, parseNs . trim $ hand!!0, parseNs . trim $ hand!!1)
+        trim = reverse . dropWhile (== ' ') . reverse . dropWhile (== ' ')
 
 parseNs :: String -> [Int]
 parseNs = fromRight (error "parse fail2") . parse pNs ""
     where
-        pNs = do
-            --_ <- spaces
-            l <- (read <$> (spaces >> many1 digit)) `sepBy` string " "
-            return l
-
-trim xs = dropSpaceTail "" $ dropWhile (== ' ') xs
-
-dropSpaceTail maybeStuff "" = ""
-dropSpaceTail maybeStuff (x:xs)
-        | x == ' ' = dropSpaceTail (x:maybeStuff) xs
-        | null maybeStuff = x : dropSpaceTail "" xs
-        | otherwise       = reverse maybeStuff ++ x : dropSpaceTail "" xs
+        pNs = (read <$> (spaces >> many1 digit)) `sepBy` string " "
--- day04/day4p2.hs
+++ day04/day4p2.hs
@@ -8,9 +8,9 @@ main :: IO ()
 main = interact $ show . numCards . map (score . parseline) . lines
 
 numCards :: [Int] -> Int
-numCards = fst . foldl fun (0, repeat 1)
+numCards = fst . foldl numCards' (0, repeat 1)
     where
-        fun (count, a:as) score = (count + a, zipWith (+) (replicate score a ++ [0, 0 ..]) as)
+        numCards' (count, a:as) score = (count + a, zipWith (+) (replicate score a ++ repeat 0) as)
 
 score :: (Int, [Int], [Int]) -> Int
 score (i, hand, winners) =
@@ -22,25 +22,14 @@ parseline :: String -> (Int, [Int], [Int])
 parseline = fromRight (error "parse fail") . parse round ""
     where
         round = do
-            _ <- string "Card "
-            _ <- spaces
+            string "Card" >> spaces
             i <- read <$> many1 digit
-            _ <- string ": "
+            string ": "
             hand <- many1 (noneOf "|") `sepBy`string "|"
             pure (i, parseNs . trim $ hand!!0, parseNs . trim $ hand!!1)
+        trim = reverse . dropWhile (== ' ') . reverse . dropWhile (== ' ')
 
 parseNs :: String -> [Int]
 parseNs = fromRight (error "parse fail2") . parse pNs ""
     where
-        pNs = do
-            --_ <- spaces
-            l <- (read <$> (spaces >> many1 digit)) `sepBy` string " "
-            return l
-
-trim xs = dropSpaceTail "" $ dropWhile (== ' ') xs
-
-dropSpaceTail maybeStuff "" = ""
-dropSpaceTail maybeStuff (x:xs)
-        | x == ' ' = dropSpaceTail (x:maybeStuff) xs
-        | null maybeStuff = x : dropSpaceTail "" xs
-        | otherwise       = reverse maybeStuff ++ x : dropSpaceTail "" xs
+        pNs = (read <$> (spaces >> many1 digit)) `sepBy` string " "

3242dea7f724f968e1bc68c8a6ff4a1fec478cf7
Fredrik Furufors <furufors@gmail.com>
2023-12-07 11:57:59 +0100

    Clean up day 7

--- day07/day7p1.hs
+++ day07/day7p1.hs
@@ -6,7 +6,6 @@ type Card = Char
 type Hand = [Card]
 data HandType = HighCard | OnePair | TwoPair | ThreeOfAKind | FullHouse | FourOfAKind | FiveOfAKind deriving (Eq, Ord)
 
-
 main :: IO ()
 main = interact $ show . score . sortBy (\(a,b) (c,d) -> cmpCards a c) . map parseIn . lines
 
@@ -18,15 +17,6 @@ parseIn  s =
 
 score :: [(Hand, Int)] -> Int
 score hs = sum $ zipWith (*) (map snd hs) [1..]
-{-
-ive of a kind, where all five cards have the same label: AAAAA
-Four of a kind, where four cards have the same label and one card has a different label: AA8AA
-Full house, where three cards have the same label, and the remaining two cards share a different label: 23332
-Three of a kind, where three cards have the same label, and the remaining two cards are each different from any other card in the hand: TTT98
-Two pair, where two cards share one label, two other cards share a second label, and the remaining card has a third label: 23432
-One pair, where two cards share one label, and the other three cards have a different label from the pair and each other: A23A4
-High card, where all cards' labels are distinct: 23456
--}
 
 cmpCards :: Hand -> Hand -> Ordering
 cmpCards a b = case compare (toCategory a) (toCategory b) of
@@ -51,7 +41,6 @@ toCategory c
         isThreeOfAkind = 3 `elem` grouped
         isTwoPair      = (==2) . length $ filter (==2) grouped
         isOnePair      = 2 `elem` grouped
-
 
 higherCardsThan :: Hand -> Hand -> Ordering
 higherCardsThan [    ] ______ = EQ

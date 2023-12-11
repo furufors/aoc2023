25be1e67ce8e488b1a3e722bc46eb686b7e576da
Fredrik Furufors <furufors@gmail.com>
2023-12-11 06:44:48 +0100

    Added day 11

--- day11/day11p1.hs
+++ day11/day11p1.hs
@@ -1,4 +1,29 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
+import Data.List
+type Pos = (Int,Int)
+
 main :: IO ()
-main = undefined
+main = interact $ show . sum . manhattans . toCoords . expand . lines
+
+
+manhattans :: [Pos] -> [Int]
+manhattans [] = []
+manhattans (a:as) = [ manhattan a b | b <- as ] ++ manhattans as
+
+manhattan :: Pos -> Pos -> Int
+manhattan (a,b) (c,d) = abs (a-c) + abs (b-d)
+
+toCoords :: [String] -> [Pos]
+toCoords ss =
+    let xmax = length (head ss) - 1
+        ymax = length ss - 1
+    in [ (x,y) | x <- [0..xmax], y <- [0..ymax], let c = ss!!y!!x, c == '#' ]
+
+expand :: [String] -> [String]
+expand = transpose . doubleEmpty . transpose . doubleEmpty
+
+doubleEmpty :: [String] -> [String]
+doubleEmpty [] = []
+doubleEmpty (a:as) | all (== '.') a = a:a:(doubleEmpty as)
+doubleEmpty (a:as) = a:(doubleEmpty as)
--- day11/day11p2.hs
+++ day11/day11p2.hs
@@ -1,4 +1,28 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
+import Data.List
+type Pos = (Int,Int)
+
 main :: IO ()
-main = undefined
+main = interact $ show . sum . (\i -> manhattans i $ toCoords i) . lines
+
+manhattans :: [String] -> [Pos] -> [Int]
+manhattans ss [    ] = []
+manhattans ss (a:as) = [ manhattan a b | b <- as ] ++ manhattans ss as
+    where
+        manhattan :: Pos -> Pos -> Int
+        manhattan (a,b) (c,d) =
+            abs (a-c) + abs (b-d) + emptyBetween b d ss + emptyBetween a c (transpose ss)
+
+emptyBetween :: Int -> Int -> [String] -> Int
+emptyBetween a b ss =
+    let u = max a b
+        l = min a b
+        d = u - l
+    in (*(999999)) . length . filter (all (== '.')) . take (d-1) . drop (l+1) $ ss
+
+toCoords :: [String] -> [Pos]
+toCoords ss =
+    let xmax = length (head ss) - 1
+        ymax = length ss - 1
+    in [ (x,y) | x <- [0..xmax], y <- [0..ymax], let c = ss!!y!!x, c == '#' ]

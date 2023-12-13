3d88bd5669151cbaea76021659380bfdd9465ac1
Fredrik Furufors <furufors@gmail.com>
2023-12-13 22:29:23 +0100

    Cleaned up day 13

--- day13/day13p1.hs
+++ day13/day13p1.hs
@@ -1,5 +1,6 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
+import Safe
 import Data.List
 import Data.List.Split
 
@@ -7,13 +8,15 @@ main :: IO ()
 main = interact $ show . sum . map (findMirror . lines) . splitOn "\n\n"
 
 findMirror :: [[Char]] -> Int
-findMirror m = let horiz = findMirror' (transpose m)
-                   vert = findMirror' m
-                   findMirror' m = let r = length (head m) - 1
-                                       cond :: Int -> Bool
-                                       cond n = all (\row -> all id $ zipWith (==) (reverse $ take n row) (drop n row)) m
-                                   in [ i |  i <- [1..r], cond i]
-               in case (horiz,vert) of
-                       ([  ], [  ]) -> 0
-                       (a:as,    _) -> 100 * a
-                       (_   , b:bs) -> b
+findMirror m =
+    let horiz = headMay $ findMirror' (transpose m)
+        vert  = headMay $ findMirror' m
+        findMirror' m =
+            let r = length (head m) - 1
+                cond :: Int -> Bool
+                cond i = all (\row -> all id $ zipWith (==) (reverse $ take i row) (drop i row)) m
+            in [ i |  i <- [1..r], cond i]
+    in case (horiz,vert) of
+        (Just a, _) -> 100 * a
+        (_, Just b) -> b
+        otherwise   -> 0

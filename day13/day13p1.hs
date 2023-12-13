2ccf69f215f8ef5caa4055bf701772bff5672a42
Fredrik Furufors <furufors@gmail.com>
2023-12-13 22:04:23 +0100

    Added day 13

--- day13/day13p1.hs
+++ day13/day13p1.hs
@@ -1,4 +1,19 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
+import Data.List
+import Data.List.Split
+
 main :: IO ()
-main = undefined
+main = interact $ show . sum . map (findMirror . lines) . splitOn "\n\n"
+
+findMirror :: [[Char]] -> Int
+findMirror m = let horiz = findMirror' (transpose m)
+                   vert = findMirror' m
+                   findMirror' m = let r = length (head m) - 1
+                                       cond :: Int -> Bool
+                                       cond n = all (\row -> all id $ zipWith (==) (reverse $ take n row) (drop n row)) m
+                                   in [ i |  i <- [1..r], cond i]
+               in case (horiz,vert) of
+                       ([  ], [  ]) -> 0
+                       (a:as,    _) -> 100 * a
+                       (_   , b:bs) -> b

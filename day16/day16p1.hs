c054c9d21ee38d52a1bcd068b3a4665ceccbc4ad
Fredrik Furufors <furufors@gmail.com>
2023-12-16 13:01:08 +0100

    Clean up day16

--- day16/day16p1.hs
+++ day16/day16p1.hs
@@ -1,49 +1,41 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
-import qualified Data.Map as M
 import qualified Data.Set as S
-type Energy = M.Map (Int,Int) Int
 data Dir = U | D | L | R deriving (Eq,Ord)
+type Energized = S.Set (Int,Int)
 type Visited = S.Set ((Int,Int),Dir)
 
 main :: IO ()
-main = interact $ show . run [((-1,0),R)] S.empty M.empty . lines
+main = interact $ show . run [((-1,0),R)] S.empty S.empty . lines
 
-run :: [((Int,Int),Dir)] -> Visited -> Energy -> [[Char]] -> Int
-run [] v e m = length . filter (>0) $ M.elems e
+run :: [((Int,Int),Dir)] -> Visited -> Energized -> [[Char]] -> Int
+run [] v e m = S.size e
 run (a:as) v e m =
-    let (pos0, spd0) = a
-        pos1@(x1,y1) = calc pos0 spd0
-        maxX = length (head m)
-        maxY = length m
-        e' = (energize (x1,y1) e)
-        v' = S.insert (pos0,spd0) v
+    let (pos0, dir) = a
+        pos1@(x1,y1) = calc pos0 dir
+        e' = S.insert pos1 e
+        v' = S.insert (pos0, dir) v
         next = case m!!y1!!x1 of
-                '.' -> (pos1, spd0):as
-                '|' -> if spd0 == U || spd0 == D
-                       then (pos1, spd0):as
+                '.' -> (pos1, dir):as
+                '|' -> if dir == U || dir == D
+                       then (pos1, dir):as
                        else [(pos1, U),(pos1, D)] ++ as
-                '/' -> case spd0 of
+                '/' -> case dir of
                         U -> (pos1, R):as
                         D -> (pos1, L):as
                         L -> (pos1, D):as
                         R -> (pos1, U):as
-                '-' -> if spd0 == L || spd0 == R
-                       then (pos1, spd0):as
+                '-' -> if dir == L || dir == R
+                       then (pos1, dir):as
                        else [(pos1, L),(pos1, R)] ++ as
-                '\\' -> case spd0 of
+                '\\' -> case dir of
                          U -> (pos1, L):as
                          D -> (pos1, R):as
                          L -> (pos1, U):as
                          R -> (pos1, D):as
-    in if 0 <= x1 && x1 < maxX && 0 <= y1 && y1 < maxY && not (S.member (pos0, spd0) v)
+    in if 0 <= x1 && x1 < (length (head m)) && 0 <= y1 && y1 < (length m) && not (S.member (pos0, dir) v)
        then run next v' e' m
        else run as v e m
-
-energize :: (Int,Int) -> Energy -> Energy
-energize p e = case M.lookup p e of
-    Nothing -> M.insert p 1 e
-    Just a -> M.insert p (a+1) e
 
 calc :: (Int,Int) -> Dir -> (Int,Int)
 calc (a,b) U = (a, b - 1)

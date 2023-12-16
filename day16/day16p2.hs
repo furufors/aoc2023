58d179a2f7779241f5f0479277ca03a3a0360322
Fredrik Furufors <furufors@gmail.com>
2023-12-16 12:44:34 +0100

    Clean up day16

--- day16/day16p2.hs
+++ day16/day16p2.hs
@@ -16,36 +16,35 @@ run2 m = maximum $ fromL ++ fromR ++ fromU ++ fromD
         fromU = [run [((x,-1),D)] S.empty M.empty m | x <- [0..(length (head m))] ]
         fromD = [run [((x,length m),U)] S.empty M.empty m | x <- [0..(length (head m))] ]
 
-
 run :: [((Int,Int),Dir)] -> Visited -> Energy -> [[Char]] -> Int
 run [] v e m = length . filter (>0) $ M.elems e
 run (a:as) v e m =
     let (pos0, spd0) = a
-        pos1 = calc pos0 spd0
-        (x1,y1) = pos1
+        pos1@(x1,y1) = calc pos0 spd0
         maxX = length (head m)
         maxY = length m
         e' = (energize (x1,y1) e)
         v' = S.insert (pos0,spd0) v
+        next = case m!!y1!!x1 of
+                '.' -> (pos1, spd0):as
+                '|' -> if spd0 == U || spd0 == D
+                       then (pos1, spd0):as
+                       else [(pos1, U),(pos1, D)] ++ as
+                '/' -> case spd0 of
+                        U -> (pos1, R):as
+                        D -> (pos1, L):as
+                        L -> (pos1, D):as
+                        R -> (pos1, U):as
+                '-' -> if spd0 == L || spd0 == R
+                       then (pos1, spd0):as
+                       else [(pos1, L),(pos1, R)] ++ as
+                '\\' -> case spd0 of
+                         U -> (pos1, L):as
+                         D -> (pos1, R):as
+                         L -> (pos1, U):as
+                         R -> (pos1, D):as
     in if 0 <= x1 && x1 < maxX && 0 <= y1 && y1 < maxY && not (S.member (pos0, spd0) v)
-       then case m!!y1!!x1 of
-            '.' -> run ((pos1, spd0):as) v' e' m
-            '|' -> if spd0 == U || spd0 == D
-                   then run ((pos1, spd0):as) v' e' m
-                   else run ([(pos1, U),(pos1, D)] ++ as) v' e' m
-            '/' -> case spd0 of
-                    U -> run ((pos1, R):as) v' e' m
-                    D -> run ((pos1, L):as) v' e' m
-                    L -> run ((pos1, D):as) v' e' m
-                    R -> run ((pos1, U):as) v' e' m
-            '-' -> if spd0 == L || spd0 == R
-                   then run ((pos1, spd0):as) v' e' m
-                   else run ([(pos1, L),(pos1, R)] ++ as) v' e' m
-            '\\' -> case spd0 of
-                     U -> run ((pos1, L):as) v' e' m
-                     D -> run ((pos1, R):as) v' e' m
-                     L -> run ((pos1, U):as) v' e' m
-                     R -> run ((pos1, D):as) v' e' m
+       then run next v' e' m
        else run as v e m
 
 energize :: (Int,Int) -> Energy -> Energy

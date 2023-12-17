e4a08688c547a452f8df20382a0e29fe1ef9db56
Fredrik Furufors <furufors@gmail.com>
2023-12-17 19:50:18 +0100

    Clean up day17

--- day17/day17p1.hs
+++ day17/day17p1.hs
@@ -11,8 +11,7 @@ main = interact $ show . fromJust . solve . map (map $ (\x -> read [x])) . lines
 solve :: [[Int]] -> Maybe (Int, [State])
 solve key = dijkstra next cost solved start
     where
-        maxX = length (head key) - 1
-        maxY = length key - 1
+        (maxX, maxY) = (length (head key) - 1, length key - 1)
         solved :: State -> Bool
         solved (x,y,h,v) = (x,y) == (maxX, maxY)
         start :: State
@@ -20,21 +19,16 @@ solve key = dijkstra next cost solved start
         cost :: State -> State -> Int
         cost _ (x,y,_,_) = key!!y!!x
         next :: State -> [State]
-        next (x,y,d,n) = filter (valid d) [(x,y+1,D,(ct D d n))
-                                          ,(x,y-1,U,(ct U d n))
-                                          ,(x+1,y,R,(ct R d n))
-                                          ,(x-1,y,L,(ct L d n))]
+        next (x,y,d,n) = filter (valid d) [(x,y+1,D, ct D d n)
+                                          ,(x,y-1,U, ct U d n)
+                                          ,(x+1,y,R, ct R d n)
+                                          ,(x-1,y,L, ct L d n)]
         ct :: Dir -> Dir -> Int -> Int
         ct d1 d2 n = if d1 == d2 then n + 1 else 1
         opposite :: Dir -> Dir -> Bool
-        opposite U D = True
-        opposite D U = True
-        opposite L R = True
-        opposite R L = True
-        opposite a b = False
+        opposite a b = (a,b) `elem` [(U,D),(D,U),(L,R),(R,L)]
         valid :: Dir -> State -> Bool
-        valid o (x,y,d,c) = x >= 0 && x <= maxX
+        valid o (x,y,d,c) =  x >= 0 && x <= maxX
                           && y >= 0 && y <= maxY
                           && c <= 3
                           && not (opposite o d)
-
--- day17/day17p2.hs
+++ day17/day17p2.hs
@@ -11,8 +11,7 @@ main = interact $ show . fromJust . solve . map (map $ (\x -> read [x])) . lines
 solve :: [[Int]] -> Maybe (Int, [State])
 solve key = dijkstra next cost solved start
     where
-        maxX = length (head key) - 1
-        maxY = length key - 1
+        (maxX, maxY) = (length (head key) - 1, length key - 1)
         solved :: State -> Bool
         solved (x,y,h,v) = (x,y) == (maxX, maxY)
         start :: State
@@ -22,26 +21,18 @@ solve key = dijkstra next cost solved start
         next :: State -> [State]
         next (0,0,_,0) = filter (valid N) [(0,1,D,1),(1,0,R,1)]
         next (x,y,d,n) | n < 4 = filter (valid d) [(x + dx d,y + dy d,d,n+1)]
-        next (x,y,d,n) = filter (valid d) [(x,y+1,D,(ct D d n))
-                                          ,(x,y-1,U,(ct U d n))
-                                          ,(x+1,y,R,(ct R d n))
-                                          ,(x-1,y,L,(ct L d n))]
-        dx :: Dir -> Int
+        next (x,y,d,n) = filter (valid d) [(x + dx d', y + dy d', d', ct d' d n) | d' <- [U,D,L,R]]
+        dx, dy :: Dir -> Int
         dx L = -1
         dx R = 1
         dx _ = 0
-        dy :: Dir -> Int
         dy U = -1
         dy D = 1
         dy _ = 0
         ct :: Dir -> Dir -> Int -> Int
         ct d1 d2 n = if d1 == d2 then n + 1 else 1
         opposite :: Dir -> Dir -> Bool
-        opposite U D = True
-        opposite D U = True
-        opposite L R = True
-        opposite R L = True
-        opposite a b = False
+        opposite a b = (a,b) `elem` [(U,D),(D,U),(L,R),(R,L)]
         valid :: Dir -> State -> Bool
         valid o (x,y,d,c) = x >= 0 && x <= maxX
                           && y >= 0 && y <= maxY
